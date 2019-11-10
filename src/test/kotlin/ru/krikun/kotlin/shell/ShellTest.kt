5d218c0f1f14748c1b64dec6a1598a859646ea6f
OlegKrikun <oleg.krikun@gmail.com>
2019-11-10 19:28:14 +0300

    Update test

--- src/test/kotlin/ru/krikun/kotlin/shell/ShellTest.kt
+++ src/test/kotlin/ru/krikun/kotlin/shell/ShellTest.kt
@@ -1,3 +1,5 @@
+@file:Suppress("ComplexRedundantLet")
+
 package ru.krikun.kotlin.shell
 
 import org.junit.Test
@@ -7,12 +9,23 @@ import kotlin.test.assertTrue
 
 class ShellTest {
     private val dir = createTempDir().apply { deleteOnExit() }
+    private val exitCodeCheck: (Int?) -> Unit = { assertEquals(0, it) }
 
     @Test
-    fun `regular shell call`() = shell(dir) {
+    fun `single shell call`() = shell(dir) {
         val result = call("echo foo").lines()
         assertNotNull(result)
         assertTrue(result.size == 1, result.toString())
         assertEquals("foo", result.first())
-    }.let { assertEquals(0, it) }
+    }.let(exitCodeCheck)
+
+    @Test
+    fun `git shell call`() = shell(dir) {
+        "git clone git@github.com:OlegKrikun/kotlin-shell.git"().let(exitCodeCheck)
+        "cd kotlin-shell"().let(exitCodeCheck)
+        val result = call("ls").lines()
+        assertNotNull(result)
+        assertTrue(result.any { it == "README.md" })
+    }.let(exitCodeCheck)
+
 }
